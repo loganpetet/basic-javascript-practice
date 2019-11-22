36f929b4964b76aca6b4d5bb3a28fc5a752cdb82
loganpetet <56040529+loganpetet@users.noreply.github.com>
2019-11-21 21:00:05 -0700

    Add files via upload

--- pokemon.js
+++ pokemon.js
@@ -8,6 +8,37 @@ class Pokemon {
 
 const Logemon = new Pokemon(800, 'Logemon', 123);
 
+document.querySelector('#getHP').addEventListener('click', getHP(25))
+
+function getHP(pokemonID) {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
+        .then(element => {
+            //console.log(muddsy.stats[5].stat.name)
+            const HP = element.stats.find(element => {
+                return element.stat.name === "hp"
+            })
+            console.log(HP.base_stat)
+            return HP.base_stat
+        })
+
+
+}
+
+document.querySelector('#pokeButton').addEventListener('click', () => {
+    let pokeId = prompt("Provide a Pokemon ID you want to add:")
+    let pokeIdNum = parseInt(pokeId, 10)
+    console.log(pokeId)
+    if (pokeIdNum > 807) {
+        alert('That Pokemon ID does not exist! Please enter a different one.')
+        return
+    } else {
+        getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokeId}`).then(result => {
+            populateDOM(result)
+        })
+            .catch(error => console.log(error))
+    }
+})
+
 async function getAPIData(url) {
     try {
         const response = await fetch(url)
@@ -17,9 +48,8 @@ async function getAPIData(url) {
         console.error(error)
     }
 }
-//data.stats[0.base_stat
 
-const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/').then(data => {
+const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/?limit=25').then(data => {
     for (const pokemon of data.results) {
         getAPIData(pokemon.url).then(pokedata => {
             populateDOM(pokedata)
@@ -48,19 +78,21 @@ function populateDOM(single_pokemon) {
     mainArea.appendChild(pokeScene)
 
     pokeCard.addEventListener('click', function () {
-        pokeCard.classList.toggle('is-flipped');
+        pokeCard.classList.toggle('is-flipped')
+
     });
 }
 
 function fillCardFront(pokeFront, data) {
-    pokeFront.setAttribute('class', 'charDivs card__face card__face--front')
+    pokeFront.setAttribute('class', 'card__face card__face--front')
     let name = document.createElement('p')
     let pic = document.createElement('img')
     pic.setAttribute('class', 'picDivs')
     let pokeNum = getPokeNumber(data.id)
     pokeFront.appendChild(name)
-    name.textContent = `${data.name} height: ${data.height}`
-    pic.src = `../images/${pokeNum}.png`
+    //name.textContent = `${data.name} height: ${data.height}`
+    //pic.src = `../images/${pokeNum}.png`
+    pic.src = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeNum}.png`
 
     pokeFront.appendChild(pic)
     pokeFront.appendChild(name)
@@ -72,7 +104,7 @@ function fillCardBack(pokeBack, data) {
     let pokeOrder = document.createElement('p')
     let pokeHP = document.createElement('h5')
     pokeOrder.textContent = `#${data.id} ${data.name[0].toUpperCase()}${data.name.slice(1)}`
-    //pokeHP.textContent = data.stats[0].base_stat
+    pokeHP.textContent = data.hp
     pokeBack.appendChild(pokeOrder)
     pokeBack.appendChild(pokeHP)
 
